ceaa250f9e6630217814872402df232788e2431e
TrAnd <magtuxgit@gmail.com>
2017-04-25 15:02:51 +0300

    Initial Commit

---
+++ OnlinicoTest/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  OnlinicoTest
+//
+//  Created by Andrij Trubchanin on 4/25/17.
+//  Copyright © 2017 Andrij Trubchanin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ OnlinicoTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OnlinicoTest
+//
+//  Created by Andrij Trubchanin on 4/25/17.
+//  Copyright © 2017 Andrij Trubchanin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
