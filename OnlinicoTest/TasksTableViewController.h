e27779e2941ad36dc1d57124a20ff822c3e5816d
TrAnd <magtuxgit@gmail.com>
2017-04-28 15:36:23 +0300

    added TasksTableVC, builded CoreDataModel

---
+++ OnlinicoTest/TasksTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TasksTableViewController.h
+//  OnlinicoTest
+//
+//  Created by Andrij Trubchanin on 4/28/17.
+//  Copyright © 2017 Andrij Trubchanin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TasksTableViewController : UITableViewController
+
+@end
